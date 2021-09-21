c3b5b78f5058a0421e349a53f66791b48ca816a3
preposition17 <yupich17@gmail.com>
2021-09-21 21:47:45 +0500

    Reformat to package

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+from ._utils import get_box_url_from_id
+from ._utils import get_sales_list
+from ._utils import print_dict
+from ._utils import print_table
+from ._utils import get_cookies
+from ._utils import get_proxy_list
+from ._utils import proxy_generator
+from ._utils import get_time
+
